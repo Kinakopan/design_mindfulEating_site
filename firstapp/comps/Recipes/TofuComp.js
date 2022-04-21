ab34e326120e38cc7bc3d108e9dedf60b518c467
HazelBCIT <ywang600@my.bcit.ca>
2022-04-21 00:04:49 -0700

    recipes page main content

--- firstapp/comps/Recipes/TofuComp.js
+++ firstapp/comps/Recipes/TofuComp.js
@@ -1,45 +1,56 @@
 import styled from 'styled-components';
 
 const StyledTofuComp = styled.div`
-display:block;
 color:${props=>props.text_color};
-font-family: M PLUS 1;
+font-family: 'm plus 1', sans-serif;
 `;
 
-const IntroBox = styled.section`
+const IntroBox = styled.span`
 background-color:${props=>props.color}; 
 position:absolute;
 right:30px;
 height:250px;
 width:250px;
 padding:10px;
-border-radius: 20px;
+border-radius:20px;
 text-align:center;
 `;
 
 const Image = styled.img`
 position:absolute;
 right:75px;
-top:280px;
+top:310px;
 height:250px;
 width:250px;
 `;
 
 const LinkBox = styled(StyledTofuComp)`
-display:flex;
-position: absolute;
-right:10px;
+position:absolute;
+right:40px;
 top:520px;
 height:60px;
-width:300px;
+width:250px;
 text-align:right;
 `;
 
+const BtnText = styled.p`
+position:absolute;
+right:35px;
+top:40px;
+color:#A2B0D3
+
+`
+
 const CartBtn = styled(Image)`
-display:flex;
+position:absolute;
+top:60px;
+right:-10px;
 height:35px;
 width:35px;
+`
 
+const Mark = styled.a`
+color:#EF7F63
 `
 
 export default function TofuComp({
@@ -57,7 +68,7 @@ export default function TofuComp({
     <Image src={img} />
 
     <LinkBox>
-      <p>Get the fresh tofu at your local market</p>
+      <BtnText>Get the fresh <Mark>tofu</Mark> at your local market</BtnText>
       <CartBtn src="/cartBtn.png" />
     </LinkBox>
 
--- firstapp/pages/recipes.js
+++ firstapp/pages/recipes.js
@@ -3,9 +3,16 @@ import RecipeCard from "../comps/Recipes/RecipeCard";
 import TofuComp from "../comps/Recipes/TofuComp";
 
 import styled from 'styled-components';
+
 const StyledRecipes = styled.div`
-display:block;
+
 `;
+
+const BgChange = styled.span`
+background-color:#E0E8FD;
+margin:30px;
+`;
+
 
 export default function Recipes(){
     return (
@@ -14,27 +21,31 @@ export default function Recipes(){
 
         <TofuComp />
 
-        <RecipeCard />
+        <BgChange>
+            <RecipeCard />
+            
+            <RecipeCard 
+            img="/recipes2.png"
+            title="Tofu Bibimbap"
+            paragraph="Korean food with various vegetables mixed with rice."
+            top="820px"
+            />
         
-        <RecipeCard 
-        img="/recipes2.png"
-        title="Tofu Bibimbap"
-        paragraph="Korean food with various vegetables mixed with rice."
-        />
-       
-        <RecipeCard 
-        img="/recipes3.png"
-        title="Tofu Cutlet Curry"
-        paragraph="Korean food with various vegetables mixed with rice."
-        />
+            <RecipeCard 
+            img="/recipes3.png"
+            title="Tofu Cutlet Curry"
+            paragraph="Korean food with various vegetables mixed with rice."
+            top="1020px"
+            />
 
-        <RecipeCard 
-        img="/recipes4.png"
-        title="Mapo Tofu"
-        paragraph="Spicy Sichuan style tofu dish with rice."
-        />
-
-        <Navbar />
+            <RecipeCard 
+            img="/recipes4.png"
+            title="Mapo Tofu"
+            paragraph="Spicy Sichuan style tofu dish with rice."
+            top="1220px"
+            />   
+        </BgChange>
+        
     </StyledRecipes>
     )
 }
